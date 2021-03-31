1179f97521616128f5cb178d9ce7d4ad91690aaa
Warren <warrenjaftha16@gmail.com>
2021-03-31 17:34:09 +0200

    First commit Warren Jaftha-219005303

---
+++ src/main/java/za/ac/cput/adp/group/assignment/Employee.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adp.group.assignment;
+
+/**
+ *
+ * @author warre
+ */
+public class Employee {
+    private String role, employeename,surname;
+    
+    public String getRole()
+    {
+        return role;
+    }
+    public void setRole(String role)
+    {
+        this.role= role;
+    }
+    public String getEmployeename()
+    {
+        return employeename;
+    }
+    public void setEmployeename(String employeename)
+    {
+        this.employeename= employeename;
+    }
+    public String getSurname()
+    {
+        return surname;
+    }
+    public void setSurname(String surname)
+    {
+        this.surname=surname;
+    }
+    @Override
+    public String toString()
+    {
+        return "Employee{" + "id=" + role+ ", Employeename=" + employeename + ", surname="  + surname + "}" ; 
+    }
+    
+}
