e2714eaf11070f68c0d2d70b9d6e43514d9247b4
Marvinhope24 <80388453+Marvinhope24@users.noreply.github.com>
2021-04-01 22:02:44 +0200

    Add files via upload
    This is the new class file

---
+++ EmployDetails.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * Marvin Hope - 219445842 
+ */
+public class EmployDetails {
+    private String EmployID, EmployNum, EmpSur;
+
+    public String getEmployID() {
+        return EmployID;
+    }
+
+    public void setEmployID(String EmployID) {
+        this.EmployID = EmployID;
+    }
+
+    public String getEmployNum() {
+        return EmployNum;
+    }
+
+    public void setEmployNum(String EmployNum) {
+        this.EmployNum = EmployNum;
+    }
+
+    public String getEmpSur() {
+        return EmpSur;
+    }
+
+    public void setEmpSur(String EmpSur) {
+        this.EmpSur = EmpSur;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "EmployID=" + EmployID + ", EmployNum=" + EmployNum + ", EmpSur=" + EmpSur + '}';
+    }
+}
